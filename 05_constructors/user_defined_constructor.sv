c367f06b2238fe375a8ad6663d3b7adde1109b71
sharonanto <sharonanto06@gmail.com>
2026-05-19 00:09:36 +0530

    Added user defined constructor example

---
+++ 05_constructors/user_defined_constructor.sv
@@ -0,0 +1,27 @@
+class first;
+
+  int data;
+
+  // User-defined constructor
+  function new(input int datain = 0);
+
+    data = datain;
+
+  endfunction
+
+endclass
+
+
+module tb;
+
+  first f1;
+
+  initial begin
+
+    f1 = new(23);
+
+    $display("Data : %0d", f1.data);
+
+  end
+
+endmodule
