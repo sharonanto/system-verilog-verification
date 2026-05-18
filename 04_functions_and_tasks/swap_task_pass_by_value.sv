4d7b083871c3b3b1b4c532a1598dd68d9ce46d6d
sharonanto <sharonanto06@gmail.com>
2026-05-19 00:06:15 +0530

    Added pass by value task example

---
+++ 04_functions_and_tasks/swap_task_pass_by_value.sv
@@ -0,0 +1,33 @@
+module tb;
+
+  // Pass by value
+  task swap(input bit [1:0] a,
+            input bit [1:0] b);
+
+    bit [1:0] temp;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    $display("Value of a : %0d and b : %0d", a, b);
+
+  endtask
+
+
+  bit [1:0] a;
+  bit [1:0] b;
+
+  initial begin
+
+    a = 1;
+    b = 2;
+
+    swap(a, b);
+
+    $display("Original values");
+    $display("Value of a : %0d and b : %0d", a, b);
+
+  end
+
+endmodule
